69fae763075607574661c0f47556b869b4a76a95
Nathan Behunin <npbehunin@gmail.com>
2019-11-12 15:40:29 -0700

    Added python notes 11-12-19

---
+++ Python Notes/Python Notes 11-12-19 (Appending).py
@@ -0,0 +1,12 @@
+testScore = ['Bill', 86, 'Nancy', 94, 'Bob', 75, 'Sarah', 100)
+names=[]
+scores=[]
+
+for i in range(len(testScores)):
+    if (i%2==0):
+        scores.append(testScores[i])
+    else
+        names.apend(testScores[i])
+        
+print 'Names:', names
+print 'Scores:' scores
